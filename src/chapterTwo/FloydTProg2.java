83c56d49ff7b92034257adcaf401240f2cb38fbc
Thomas Floyd <tefloyd1215@gmail.com>
2019-09-23 11:43:29 -0500

    -Program 2 Completed

--- src/chapterTwo/FloydTProg2.java
+++ src/chapterTwo/FloydTProg2.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
  * Thomas Floyd (W#0699719)
  * CMPS 161_05
  * Dr. Bonnie Achee
- * September 16, 2019
+ * September 23, 2019
  * 
  * Summary: 
- * 
+ * 	First, a Scanner object is declared, and some double variables to hold the 
+ * 	coordinate data. Next, the program repeats a series of prompts and scanner inputs
+ *  to take in 6 double values for the coordinates. When that part is complete, 
+ *  the Scanner will close, and then return the coordinates the user entered. 
+ *  Then, the 3 side lengths of the triangle formed with the coordinates will 
+ *  be calculated and returned. Now, the area can be calculated using the 
+ *  formula specified below. That area is truncated and then returned to the user.
+ *
+ * 	area = sqrt(s(s - side1)(s - side2)(s - side3) ), where s = (side1 + side2 + side3)/2;
+ *	distance = sqrt((x1 - x2)^2 + (y1 - y2)^2) 
  */
 
 public class FloydTProg2 {
@@ -46,6 +55,7 @@ public class FloydTProg2 {
 		System.out.println("(" + x1 + ", " + y1 + ")");
 		System.out.println("(" + x2 + ", " + y2 + ")");
 		System.out.println("(" + x3 + ", " + y3 + ")");
+		System.out.println(""); //Skip Line without using \n
 		
 		//Compute Sides using Distance Formula
 		double side1 = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
@@ -56,6 +66,7 @@ public class FloydTProg2 {
 		System.out.println("Side 1 = " + side1 + " units");
 		System.out.println("Side 2 = " + side2 + " units");
 		System.out.println("Side 3 = " + side3 + " units");
+		System.out.println(""); //Skip Line without using \n
 		
 		//Compute Area
 		double s = (side1 + side2 + side3)/2;
@@ -66,5 +77,4 @@ public class FloydTProg2 {
 		area = castedArea / 100;
 		System.out.println("Area = " + area + " units^2");
 	}
-
 }
